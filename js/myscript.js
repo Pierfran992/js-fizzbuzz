706bdb16e4be36c051c8128ab8d913c6df6a4ea0
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-11-09 15:32:16 +0100

    Corretto l'ordine delle condizioni degli if. Come prima condizione è stata messa se il numero è divisibile sia per 5 sia per 3.

--- js/myscript.js
+++ js/myscript.js
@@ -5,15 +5,15 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.*/
 
 for (let i =1; i <= 100; i++){
 
-    if ( i % 3 === 0 ) { // se un numero è divisibile per 3 stampa fizz
+    if ( i % 3 == 0 && i % 5 == 0 ) { // se un numero è divisibile sia per 3 che per 5 stampa fizzbuzz
 
         console.log("fizz");
 
-    } else if ( i % 5 === 0) { // se un numero è divisibile per 5 stanpa buzz
+    } else if ( i % 3 == 0 ) { // se un numero è divisibile per 3 stampa fizz
 
         console.log("buzz");
 
-    } else if ( i % 3 === 0 && i % 5 === 0) { // se un numero è divisibile sia per 3 che per 5 stampa fizzbuzz
+    } else if ( i % 5 == 0 ) { // se un numero è divisibile per 5 stanpa buzz
 
         console.log("fizzbuzz");
 
